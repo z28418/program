2bc184522cfb72ad621aa13a1aded2ce4d1a48d5
Matt Upton-Cashmore (Z28418) <Z28418@sfc.potteries.ac.uk>
2022-05-11 12:41:48 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from valid import *
+
+
+
+try:
+    lengthCheck("abcd",4)
+except:
+    print("more parameters")
---
+++ valid.py
@@ -0,0 +1,47 @@
+#========================================
+#
+# this module is for the validation codes only
+# written by matt upton-cashmore
+# last checked 9/05/22
+# known bugs: none
+#=============================================
+
+
+
+# this function is for checking the length of our data
+# data is what we want to chack, length is our value for our max/min length
+# option is for later when decide what length im checking
+def lengthCheck(data,length,option):
+    if option ==1:
+        if len(data) == length:
+            return True
+        else:
+            return False
+    elif option == 2:
+            if len(data) >= length:
+                return True
+            else:
+                return False
+
+
+
+
+
+
+# this function checks the data is within the correct length parameters
+# data is what we want to to validate, high and low are our max/min values
+
+def rangeCheck(data,hi,low):
+    if (int(len(data)) > low) and (int(len(data)) < hi):
+        return True
+    else:
+        return False
+    #return true or false
+
+
+
+if __name__ == "__main__":
+    pass
+
+
+
